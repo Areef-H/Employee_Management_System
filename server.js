eaf5ae64b070548ab2ff288f29ee771b6f4487a3
Areef Hossain <areefhossain@dhcp-39-107-6.dhcpint.csulb.edu>
2024-08-13 11:46:57 -0700

    Basic JWT authetication filter

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const posts = [
+    {
+    username: 'Kyle',
+    title: 'Post 1'
+
+    },
+    {
+        username: 'Jim',
+        title: 'Post 1'
+    }
+]
+app.get('/posts', (req, res) => {
+
+})
+app.listen(3000)
+// tues jwt
+// wed jwt
+// thurs dashboard --Ronaldo project 
+// friday info/inheritance/abstract -- Ronaldo project
+// sat info add-remove -- Ronaldo Project
+// sun resume
